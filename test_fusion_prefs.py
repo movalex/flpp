3c43b054a329477b1f4964bce0442fed84edd572
Alexey Bogomolov <mail@abogomolov.com>
2023-04-05 00:36:55 +0300

    use utf-8 encoding

--- test_fusion_prefs.py
+++ test_fusion_prefs.py
@@ -3,15 +3,15 @@ import json
 
 FILE = "fusion_shared.prefs"
 
-with open(str(FILE), "r") as f:
+with open(str(FILE), "r", encoding='utf-8') as f:
     text = f.read()
 
 data = slpp.decode(text)
-print(data)
+# print(data)
 
 data["Locked"] = False
-with open("fusion_shared_modified.prefs", "w") as out:
+with open("fusion_shared_modified.prefs", "w", encoding='utf-8') as out:
     out.write(slpp.encode(data))
 
-with open("fusion_shared_modified.json", "w") as out:
+with open("fusion_shared_modified.json", "w", encoding='utf-8') as out:
     json.dump(data, out, indent=4, sort_keys=False)
