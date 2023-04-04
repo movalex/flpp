d78782592de9eb6842d242c0bbb7746d2b2ad235
Alexey Bogomolov <mail@abogomolov.com>
2023-04-04 20:06:41 +0300

    write both json and prefs

--- test_fusion_prefs.py
+++ test_fusion_prefs.py
@@ -1,4 +1,5 @@
 from slpp import slpp
+import json
 
 FILE = "fusion_shared.prefs"
 
@@ -9,5 +10,8 @@ data = slpp.decode(text)
 print(data)
 
 data["Locked"] = False
-with open("fusion_shared_out.prefs", "w") as out:
+with open("fusion_shared_modified.prefs", "w") as out:
     out.write(slpp.encode(data))
+
+with open("fusion_shared_modified.json", "w") as out:
+    json.dump(data, out, indent=4, sort_keys=False)
