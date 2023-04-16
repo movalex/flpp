93375a3d9ab74193faec5feb195eff4d5eddcf23
Alexey Bogomolov <mail@abogomolov.com>
2023-04-17 01:42:42 +0300

    update prefs parsing test

--- test_fusion_prefs.py
+++ test_fusion_prefs.py
@@ -4,19 +4,16 @@ from pprint import pprint
 from slpp import slpp
 from pathlib import Path
 
-FILE = "fusion_shared.prefs"
-# FILE = "Fusion.prefs"
+FILE = "Fusion.prefs"
 
 file_name = Path(FILE).stem
 extension = Path(FILE).suffix
 
-
 with open(str(FILE), "r", encoding="utf-8") as f:
     text = f.read()
 
-
 data = slpp.decode(text)
-data["Locked"] = False
+# data["Locked"] = False
 
 with open(f"{file_name}_modified{extension}", "w", encoding="utf-8") as out:
     upd = slpp.encode(data)
