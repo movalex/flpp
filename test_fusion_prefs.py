63ced78082bad086e12595e4ed234f93f168b535
Alexey Bogomolov <mail@abogomolov.com>
2023-04-17 11:06:20 +0300

    update tests

--- test_fusion_prefs.py
+++ test_fusion_prefs.py
@@ -14,10 +14,10 @@ with open(str(FILE), "r", encoding="utf-8") as f:
 
 data = flpp.decode(text)
 # data["Locked"] = False
+with open(f"{file_name}_modified.json", "w", encoding="utf-8") as out:
+    json.dump(data, out, indent=4, sort_keys=False)
 
 with open(f"{file_name}_modified{extension}", "w", encoding="utf-8") as out:
     upd = flpp.encode(data)
     out.write(upd)
 
-with open(f"{file_name}_modified.json", "w", encoding="utf-8") as out:
-    json.dump(data, out, indent=4, sort_keys=False)
