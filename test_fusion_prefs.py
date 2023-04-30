3f90dfdb6d47b164142152699fee4d72169ea1cd
Alexey Bogomolov <mail@abogomolov.com>
2023-04-30 03:21:12 +0300

    add fusion comp parsing test

--- test_fusion_prefs.py
+++ test_fusion_prefs.py
@@ -4,20 +4,27 @@ from pprint import pprint
 from flpp import flpp
 from pathlib import Path
 
-FILE = "Fusion.prefs"
+FILE = "Fusion.comp"
 
 file_name = Path(FILE).stem
 extension = Path(FILE).suffix
 
 with open(str(FILE), "r", encoding="utf-8") as f:
-    text = f.read()
+    if extension == ".comp":
+        lines = f.readlines()
+        lines[0] = "{\n"
+        text = "\n".join(lines)
+    else:
+        text = f.read()
 
 data = flpp.decode(text)
 # data["Locked"] = False
+with open(f"{file_name}_modified.json", "w", encoding="utf-8") as out:
+    json.dump(data, out, indent=4, sort_keys=False)
 
 with open(f"{file_name}_modified{extension}", "w", encoding="utf-8") as out:
+    if extension == ".comp":
+        print("Composition", file=out, end="")
     upd = flpp.encode(data)
     out.write(upd)
 
-with open(f"{file_name}_modified.json", "w", encoding="utf-8") as out:
-    json.dump(data, out, indent=4, sort_keys=False)
