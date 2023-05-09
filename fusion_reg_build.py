ad0e649cf92920e1ab07298b300202a9ca92d117
Alexey Bogomolov <mail@abogomolov.com>
2023-05-10 02:25:13 +0300

    add fusion registry builder script

---
+++ fusion_reg_build.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+try:
+    import DaVinciResolveScript as bmd
+except ImportError:
+    print(
+        "RESOLVE_SCRIPT_API is not set. Trying to import fusionscript directly"
+    )
+    try:
+        import fusionscript as bmd
+    except ImportError:
+        print(
+            "Unable to find Fusion script module."
+            "Check the README and update PYTHONPATH accordingly"
+        )
+        sys.exit()
+
+fu = bmd.scriptapp("Fusion")
+reg = [i.ID for i in fu.GetRegList().values()]
+with open("FuRegList.json", "w") as out:
+    json.dump(reg, out)
