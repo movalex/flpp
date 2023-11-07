6661559cc81a2bf89ba88d46f74b89402a58413d
Alexey Bogomolov <mail@abogomolov.com>
2023-11-07 23:15:47 +0300

    parse all sample files in folder

--- src/main/tests/parse_fusion_files.py
+++ src/main/tests/parse_fusion_files.py
@@ -1,27 +1,45 @@
 import json
 from pathlib import Path
-from flpp import flpp
+from main.flpp import flpp
 
-FILE = r"C:\Users\videopro\Documents\github\flpp\src\tests\Fusion.masterprefs"
+FOLDER = Path("src/main/tests/examples").resolve()
 
-file_name = Path(FILE).stem
-extension = Path(FILE).suffix
 
-with open(str(FILE), "r", encoding="utf-8") as f:
-    if extension == ".comp":
-        lines = f.readlines()
-        lines[0] = "{\n"
-        text = "\n".join(lines)
-    else:
-        text = f.read()
+def parse_examples(target_folder: Path):
+    output_folder = target_folder / "output"
+    output_folder.mkdir(exist_ok=True)
 
-data = flpp.decode(text)
-# data["Locked"] = False
-with open(f"{file_name}_modified.json", "w", encoding="utf-8") as out:
-    json.dump(data, out, indent=4, sort_keys=False)
+    for file in target_folder.iterdir():
+        file_name = file.stem
+        if not file_name.startswith("fusion_"):
+            continue
+        extension = file.suffix
 
-with open(f"{file_name}_modified{extension}", "w", encoding="utf-8") as out:
-    if extension == ".comp":
-        print("Composition", file=out, end="")
-    upd = flpp.encode(data)
-    out.write(upd)
+        with open(str(file), "r", encoding="utf-8") as f:
+            if extension == ".comp":
+                lines = f.readlines()
+                lines[0] = "{\n"
+                text = "\n".join(lines)
+            else:
+                text = f.read()
+
+        data = flpp.decode(text)
+        if extension == ".masterprefs":
+            data["Locked"] = False
+
+        itermediate_file = output_folder / f"{file_name}_intermediate.json"
+
+        with open(itermediate_file, "w", encoding="utf-8") as json_out:
+            json.dump(data, json_out, indent=4, sort_keys=False)
+
+        parsed_file = output_folder / f"{file_name}_parsed{extension}"
+
+        with open(parsed_file, "w", encoding="utf-8") as out:
+            if extension == ".comp":
+                print("Composition", file=out, end="")
+            upd = flpp.encode(data)
+            out.write(upd)
+
+
+if __name__ == "__main__":
+    parse_examples(FOLDER)
