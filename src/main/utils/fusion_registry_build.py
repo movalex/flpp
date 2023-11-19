800dbee726b9b7240029eee0b5d7ec21f15e360d
Alexey Bogomolov <mail@abogomolov.com>
2023-11-19 14:40:46 +0300

    canct no fusion error

--- src/main/utils/fusion_registry_build.py
+++ src/main/utils/fusion_registry_build.py
@@ -3,6 +3,7 @@ import sys
 
 try:
     import DaVinciResolveScript as bmd
+
 except ImportError:
     print("RESOLVE_SCRIPT_API is not set. Trying to import fusionscript directly")
     try:
@@ -14,7 +15,17 @@ except ImportError:
         )
         sys.exit()
 
-fu = bmd.scriptapp("Fusion")
-reg = [i.ID for i in fu.GetRegList().values()]
-with open("src/main/utils/fusion_registry_list.json", "w") as out:
-    json.dump(reg, out)
+
+def main():
+    fu = bmd.scriptapp("Fusion")
+    try:
+        reg_list = fu.GetRegList()
+        reg = [i.ID for i in reg_list.values()]
+        with open("src/main/utils/fusion_registry_list.json", "w") as out:
+            json.dump(reg, out)
+    except AttributeError:
+        print("No Fusion instance found")
+
+
+if __name__ == "__main__":
+    main()
